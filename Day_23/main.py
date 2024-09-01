db6dcf15f48f9142df0721e791d0d2be9f886e18
Roy Wilson <royawilson@hotmail.co.uk>
2024-09-01 20:57:32 +0100

    day 23 Car Manager and car movement manager working

--- Day_23/main.py
+++ Day_23/main.py
@@ -7,6 +7,7 @@ import time
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
+import car_manager
 from scoreboard import Scoreboard
 
 screen = Screen()
@@ -14,18 +15,17 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 player = Player()
 screen.listen()
-screen.onkey(player.move_player, 'Up')
+screen.onkeypress(player.move_player, 'Up')
 
+# car = CarManager()
 car = CarManager()
-car_list = []
+# car_list.append(CarManager())
 
 game_is_on = True
 while game_is_on:
-
-    car_list.append(car)
-    for item in car_list:
-        item.move_car()
-        time.sleep(0.1)
-        screen.update()
+    time.sleep(0.1)
+    screen.update()
+    car.make_car()
+    car.move_car()
 
 screen.exitonclick()
