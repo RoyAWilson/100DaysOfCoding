69e24525389c168c65039b822bd511ee64225df0
Roy Wilson <royawilson@hotmail.co.uk>
2024-09-02 21:00:51 +0100

    Day 24 Letters completed

---
+++ Day_24/main.py
@@ -0,0 +1,22 @@
+'''
+To create a mail merge? from the given letter
+and replace the [name] placeholder
+with a given name in the names file,
+saving each letter in ReadyToSend folder
+'''
+
+with open(r'./Letters/starting_letter.txt', 'r') as letter_text:
+    text_list = letter_text.readlines()
+    letter_text.close()
+with open(r'./Names/invited_names.txt') as names:
+    names_list = names.readlines()
+    names.close()
+for i in range(len(text_list)):
+    if text_list[i] == 'Dear [name],\n':
+        for names in names_list:
+            text_list[i] = f'Dear {names}'
+            # print(text_list)
+            with open(f'./ReadyToSend/{names[:-2]}.txt', 'w') as new_letters:
+                new_letters.writelines(text_list)
+
+# Done without watcing lecture.
